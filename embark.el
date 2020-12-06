41bb249025f880ecfbf0ba6eaa2a225c65c80164
Omar Antolín <omar.antolin@gmail.com>
2020-12-06 10:30:57 -0600

    Fix opening eshell in directories like ~/dir1/~/dir2/ (issue #42)
    Emacs has a convention whereby paths that "start from the top again"
    mean you discard the first part, that is, if a path contains // or
    /~/, everything up to and including the first / in // or /~/ gets
    discarded.
    
    I thought expand-file-name handled this Emacs convention, but it
    doesn't; it's handled by substitute-in-file-name, which I now added to
    embark-eshell-in-directory.

--- embark.el
+++ embark.el
@@ -1535,9 +1535,10 @@ This is whatever command opened the minibuffer in the first place."
   "Run eshell in directory of embark target."
   (interactive)
   (let ((default-directory
-          (file-name-directory
-           (expand-file-name
-            (embark-target)))))
+            (file-name-directory
+             (expand-file-name
+              (substitute-in-file-name
+               (embark-target))))))
     (eshell '(4))))
 
 (defun embark-describe-symbol ()
