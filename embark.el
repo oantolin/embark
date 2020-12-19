c169565042987524bd83b97006f92b49aa502c10
Omar AntolÃ­n Camarena <omar.antolin@gmail.com>
2020-12-19 13:32:46 -0600

    Remove embark-undefined in a way that still allows M-x for actions
    This is an alternative to pull request #59, that still keeps the
    previous freedom in specifying actions.

--- embark.el
+++ embark.el
@@ -690,8 +690,8 @@ BODY."
     ;; embark-act and cleanup is scheduled then
     (advice-add this-command :after #'embark--cleanup))
   (let ((want-current-buffer
-         (memq this-command '(ignore embark-undefined embark-occur
-                                     embark-live-occur embark-export))))
+         (memq this-command
+               '(ignore embark-occur embark-live-occur embark-export))))
     (setf (buffer-local-value 'embark--command embark--target-buffer)
           embark--command)
     (unless want-current-buffer (set-buffer embark--target-buffer))
@@ -716,7 +716,11 @@ use for the action."
                        embark-become-indicator))))
            (set-transient-map
             embark--keymap
-            (lambda () (memq this-command embark--keep-alive-list))
+            (lambda ()
+              (if (not (eq this-command #'self-insert-command))
+                  (memq this-command embark--keep-alive-list)
+                (minibuffer-message "Not an action")
+                (setq this-command #'ignore)))
             (lambda () (embark--setup-action continuep)))
            (embark--show-indicator indicator)))
         ((eq ps 'completion)
@@ -1462,11 +1466,6 @@ Returns the chosen command."
           (command
            (command-execute command)))))
 
-(defun embark-undefined ()
-  "Cancel action and show an error message."
-  (interactive)
-  (minibuffer-message "Not an action"))
-
 (defun embark-default-action ()
   "Default action.
 This is whatever command opened the minibuffer in the first place."
@@ -1602,8 +1601,7 @@ and leaves the point to the left of it."
   :parent universal-argument-map
   ("C-h" embark-keymap-help)
   ("C-u" universal-argument)
-  ("C-g" ignore)
-  ([remap self-insert-command] embark-undefined))
+  ("C-g" ignore))
 
 (embark-define-keymap embark-general-map
   "Keymap for Embark general actions."
