2e879c2e28e4fe38677a9c2d9ee13f9c7071ef53
Omar Antolín <omar.antolin@gmail.com>
2021-01-12 18:06:20 -0600

    Add more commentary to embark-consult

--- embark-consult.el
+++ embark-consult.el
@@ -25,9 +25,25 @@
 
 ;; This package provides integration between Embark and Consult.
 
-;; For now, it only provides access to Consult preview from
-;; auto-updating Embark Collect buffer that is associated to an active
-;; minibuffer for a Consult command:
+;; It contains some functionality previously contain in Embark itself:
+
+;; - Support for consult-buffer, so that you get the correct actions
+;; for each type of entry in cosult-buffer's list.
+
+;; - Support for consult-line, consult-outline, consult-mark and
+;; consult-global-mark, so that the insert and save actions don't
+;; include a weird unicode character at the start of the line, and so
+;; you can export from them to an occur buffer (where occur-edit-mode
+;; works!).
+
+;; Just load this package to get the above functionality, no further
+;; configuration is necessary.
+
+;; Additionally this package contains some functionality that has
+;; never been in Embark: access to Consult preview from auto-updating
+;; Embark Collect buffer that is associated to an active minibuffer
+;; for a Consult command. For information on Consult preview, see
+;; Consult's info manual or its readme on GitHub.
 
 ;; - `embark-consult-preview-at-point', a command to trigger Consult's
 ;; preview for the entry at point.
@@ -36,8 +52,15 @@
 ;; Collect buffers that automatically previous the entry at point as
 ;; you move around.
 
-;; Eventually all Consult-related functionality in Embark will be
-;; moved to this package.
+;; If you always want the minor mode enabled whenever it possible use:
+
+;; (add-hook 'embark-collect-mode-hook #'embark-consult-preview-minor-mode)
+
+;; If you don't want the minor mode automatically one and prefer to
+;; trigger the consult previews manually use this instead:
+
+;; (define-key embark-collect-mode-map (kbd "C-j")
+;;   #'embark-consult-preview-at-point)
 
 ;;; Code:
 
