558df851d6b9e67607e5892dce8237af01275eb6
Omar Antolín <omar.antolin@gmail.com>
2021-02-11 12:34:22 -0600

    Fix highlight toggling for future Emacs versions
    Apparently Emacs 28 will change the type of the variable
    hi-lock-interactive-patterns so that its keys are no longer regexp
    strings, and will introduce hi-lock-interactive-lighters which does
    have those keys. So let's use hi-lock-interactive-lighters if
    available and fallback to hi-lock-interactive-patterns if not.

--- embark.el
+++ embark.el
@@ -2013,11 +2013,14 @@ minibuffer, which means it can be used as an Embark action."
 (defun embark-toggle-highlight ()
   "Toggle symbol highlighting using `highlight-symbol-at-point'."
   (interactive)
-  (let ((regexp (find-tag-default-as-symbol-regexp)))
-    (if (and (boundp 'hi-lock-interactive-patterns)
-             (assoc regexp hi-lock-interactive-patterns))
-        (hi-lock-unface-buffer regexp)
-      (hi-lock-face-symbol-at-point))))
+  (let ((regexp (find-tag-default-as-symbol-regexp))
+        (highlighted (cl-find t
+                              '(hi-lock-interactive-lighters
+                                hi-lock-interactive-patterns)
+                              :key #'boundp)))
+    (if (and highlighted (assoc regexp (symbol-value highlighted)))
+        (unhighlight-regexp regexp)
+      (highlight-symbol-at-point))))
 
 ;;; setup hooks for actions
 
