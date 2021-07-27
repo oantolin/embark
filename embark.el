afe475a1f2473bcf64654d6bc17a10cc48860c04
Daniel Mendler <mail@daniel-mendler.de>
2021-07-28 01:31:23 +0200

    Add embark-target-mode

--- embark.el
+++ embark.el
@@ -2737,5 +2737,30 @@ and leaves the point to the left of it."
   ("f" flush-lines)
   ("c" count-matches))
 
+(defvar embark--target-mode-timer nil)
+(defvar embark--target-mode-string "")
+
+(defun embark--target-mode-update ()
+  (setq embark--target-mode-string
+        (if-let (targets (embark--targets))
+            (format "[%s%s] "
+                    (propertize (symbol-name (caaar targets)) 'face 'bold)
+                    (mapconcat (lambda (x) (format ", %s" (caar x)))
+                               (cdr targets)
+                               ""))
+          "")))
+
+(define-minor-mode embark-target-mode
+  "Shows the current targets in the modeline."
+  :global t
+  (setq mode-line-misc-info (assq-delete-all 'embark-target-mode mode-line-misc-info))
+  (when embark--target-mode-timer
+    (cancel-timer embark--target-mode-timer)
+    (setq embark--target-mode-timer nil))
+  (when embark-target-mode
+    (push '(embark-target-mode (:eval embark--target-mode-string)) mode-line-misc-info)
+    (setq embark--target-mode-timer
+          (run-with-idle-timer 0.1 t #'embark--target-mode-update))))
+
 (provide 'embark)
 ;;; embark.el ends here
