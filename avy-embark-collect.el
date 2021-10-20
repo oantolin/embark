9a8bb2915aa57ed2151daf31b90c9c135c2f4989
Omar Antolín <omar.antolin@gmail.com>
2021-10-19 20:11:38 -0500

    Deprecate avy-embark-collect in favor of link-hint

--- avy-embark-collect.el
+++ avy-embark-collect.el
@@ -27,6 +27,9 @@
 ;; `avy-embark-collect-act', that use avy to jump to an Embark Collect
 ;; entry and choose it or act on it, respectively.
 
+;; I no longer recommend using this package, and suggest users migrate
+;; to the excellent link-hint package by Fox Kiester.
+
 ;;; Code:
 
 (require 'avy)
