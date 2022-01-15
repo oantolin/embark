7745ad00091bbf3bdf98a2dcdbb20fc9cb05d5c1
Omar Antolín <omar.antolin@gmail.com>
2022-01-15 09:40:48 -0600

    Bump version for GNU ELPA

--- embark-consult.el
+++ embark-consult.el
@@ -5,7 +5,7 @@
 ;; Author: Omar Antolín Camarena <omar@matem.unam.mx>
 ;; Maintainer: Omar Antolín Camarena <omar@matem.unam.mx>
 ;; Keywords: convenience
-;; Version: 0.3
+;; Version: 0.4
 ;; Homepage: https://github.com/oantolin/embark
 ;; Package-Requires: ((emacs "26.1") (embark "0.12") (consult "0.10"))
 
