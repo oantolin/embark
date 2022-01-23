171f6f3731cf92fe37b6308da3787d93274ad4c2
Daanturo <daanturo@gmail.com>
2022-01-23 15:55:15 +0700

    Don't quit when the pressed key isn't presented

--- embark.el
+++ embark.el
@@ -1998,16 +1998,14 @@ target."
     (unwind-protect
         (while
             (let* ((target (car targets))
+                   (keymap (let ((embark-default-action-overrides
+                                  (if default-done
+                                      `((t . ,default-done))
+                                    embark-default-action-overrides)))
+                             (embark--action-keymap (plist-get target :type)
+                                                    (cdr targets))))
                    (action
-                    (or (embark--prompt
-                         indicators
-                         (let ((embark-default-action-overrides
-                                (if default-done
-                                    `((t . ,default-done))
-                                  embark-default-action-overrides)))
-                           (embark--action-keymap (plist-get target :type)
-                                                  (cdr targets)))
-                         targets)
+                    (or (embark--prompt indicators keymap targets)
                         (user-error "Canceled")))
                    (default-action (or default-done
                                        (embark--default-action
@@ -2032,7 +2030,14 @@ target."
                                 (eq action embark--command))
                            (embark--orig-target target)
                          target)
-                       (if embark-quit-after-action (not arg) arg))
+                       (xor
+                        ;; reverse the quitting behavior
+                        arg
+                        (and embark-quit-after-action
+                             (member action
+                                     (cl-loop
+                                      for (_ . cmd) in (embark--all-bindings keymap)
+                                      collect cmd)))))
                     (user-error
                      (funcall (if repeat #'message #'user-error)
                               "%s" (cadr err))))
