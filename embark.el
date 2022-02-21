df4cdfd497361cf65f68052c1bb31d321c902269
Omar Antolín <omar.antolin@gmail.com>
2022-02-21 11:10:09 -0600

    sort-regexp-fields should ignore the target (fix #465)

--- embark.el
+++ embark.el
@@ -423,7 +423,8 @@ replaced by the single `embark-allow-edit-actions' variable."
     (shell-command-on-region embark--ignore-target)
     (format-encode-region embark--ignore-target)
     (format-decode-region embark--ignore-target)
-    (xref-find-definitions embark--ignore-target))
+    (xref-find-definitions embark--ignore-target)
+    (sort-regexp-fields embark--ignore-target))
   "Alist associating commands with post-injection setup hooks.
 For commands appearing as keys in this alist, run the
 corresponding value as a setup hook after injecting the target
