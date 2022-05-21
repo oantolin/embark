97270d725761ee02db461b45b18ec16ae31f203e
Omar Antolín <omar.antolin@gmail.com>
2022-05-21 12:36:39 -0500

    Bump package version
    Things have quieted down a bit and I think I have all the
    functionality I wanted for 1.0 now, so I'll consider 0.17 a 1.0
    pre-release.

--- embark.el
+++ embark.el
@@ -5,7 +5,7 @@
 ;; Author: Omar Antolín Camarena <omar@matem.unam.mx>
 ;; Maintainer: Omar Antolín Camarena <omar@matem.unam.mx>
 ;; Keywords: convenience
-;; Version: 0.16
+;; Version: 0.17
 ;; Homepage: https://github.com/oantolin/embark
 ;; Package-Requires: ((emacs "26.1"))
 
