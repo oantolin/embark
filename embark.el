09a7d69b01816f5b3a190d50d6091425863ab167
Omar Antolín Camarena <omar.antolin@gmail.com>
2022-11-08 13:06:50 -0600

    eww-download-callback takes 2 args on 27.1 (fix #557)
    In Emacs 27.1 and older eww-download-callback always uses
    eww-download-directory, but in Emacs 28 it takes a second argument
    that specifies the directory. This change uses func-arity to decide
    between the calling conventions. (Luckily func-arity was added in
    Emacs 26.1, which is what Embark depends on.)

--- embark.el
+++ embark.el
@@ -3745,7 +3745,10 @@ ALGORITHM is the hash algorithm symbol understood by `secure-hash'."
   (let ((dir eww-download-directory))
     (when (functionp dir) (setq dir (funcall dir)))
     (access-file dir "Download failed")
-    (url-retrieve url #'eww-download-callback (list url dir))))
+    (url-retrieve url #'eww-download-callback
+                  (if (= (car (func-arity #'eww-download-callback)) 3)
+                      (list url dir)
+                    (list url)))))
 
 ;;; Setup and pre-action hooks
 
