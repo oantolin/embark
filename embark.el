724fd850b89337f3031fc3291c7cd0c4f289228e
Hugo Heagren <hugo@heagren.com>
2023-02-08 23:43:48 +0000

    new function: embark-target-help-symbol
    Get symbol in help buffer at point.

--- embark.el
+++ embark.el
@@ -951,6 +951,32 @@ their own target finder.  See for example
                raw)
             ,beg . ,end))))))
 
+(defun embark-target-help-symbol ()
+  "Return the subject of the current help buffer.
+
+Guess symbol's heuristically. If `help-mode--current-data'
+has a :type property then follow that. If not return 'function if
+if the symbol passes `functionp', or 'face if it passes `facep'
+or 'symbol as a default."
+  (when help-mode--current-data
+    (let* ((sym (plist-get help-mode--current-data :symbol))
+           (type
+            ;; This is not perfect but covers lots of cases
+            (cond ((alist-get
+                    (plist-get help-mode--current-data :type)
+                    '((defvar . variable)
+                      (defface . face))))
+                  ((functionp sym) 'function)
+                  ((facep sym) 'face)
+                  'symbol))
+	   (name (symbol-name sym))
+	   beg end)
+      (save-excursion
+	(goto-char 0)
+	(setq end (search-forward name))
+	(setq beg (search-backward name)))
+      `(,type ,name ,beg . ,end))))
+
 (defun embark--cycle-key ()
   "Return the key to use for `embark-cycle'."
   (if embark-cycle-key
