9e2d2a22e516f2e374c97e6099f51888f04bfc83
Omar Antolín <omar.antolin@gmail.com>
2023-10-03 22:57:21 -0600

    Treat all commands ending in self-insert-command the same

--- embark.el
+++ embark.el
@@ -1261,7 +1261,8 @@ UPDATE is the indicator update function."
                                                     ; bound, then obey
                 (and def (not (numberp def))))) ; number means "invalid prefix"
        cmd)
-      ((or 'self-insert-command 'org-self-insert-command)
+      ((and (pred symbolp)
+            (guard (string-suffix-p "self-insert-command" (symbol-name cmd))))
        (minibuffer-message "Not an action")
        (embark-keymap-prompter keymap update))
       ((or 'scroll-other-window 'scroll-other-window-down)
